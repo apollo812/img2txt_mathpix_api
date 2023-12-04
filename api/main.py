9f4178644e3d4fe7080cbdc0652940bec12b380d
apollo812 <goodluckbusiness0101@gmail.com>
2023-12-03 21:15:15 -0500

    refactor: infra

--- api/main.py
+++ api/main.py
@@ -1,32 +1,26 @@
-from typing import List, Optional
+import io
+import os
+import sys
 
-from fastapi import Depends, FastAPI
+# Get the current script's directory
+current_script_directory = os.path.dirname(os.path.abspath(__file__))
+
+# Get the project root path
+project_root = os.path.abspath(os.path.join(current_script_directory, os.pardir))
+
+# Append the project root and current script directory to the system path
+sys.path.append(project_root)
+sys.path.append(current_script_directory)
+
+from fastapi import Depends, FastAPI, File, Response, UploadFile
 from starlette.responses import RedirectResponse
 from starlette.status import HTTP_201_CREATED
 
-from api.repository import Todo, TodoFilter, TodoRepository, create_todo_repository
-
+# Create a FastAPI application
 app = FastAPI(swagger_ui_parameters={"tryItOutEnabled": True})
 
 
+# Define a route to handle the root endpoint and redirect to the API documentation
 @app.get("/")
 async def root():
     return RedirectResponse(app.docs_url)
-
-
-@app.post("/create/{key}", status_code=HTTP_201_CREATED)
-def create(key: str, value: str, todo_repository: TodoRepository = Depends(create_todo_repository)):
-    with todo_repository as repo:
-        repo.save(Todo(key=key, value=value))
-
-
-@app.get("/get/{key}", response_model=Optional[Todo])
-def get(key: str, todo_repository: TodoRepository = Depends(create_todo_repository)):
-    with todo_repository as repo:
-        return repo.get_by_key(key)
-
-
-@app.get("/find", response_model=List[Todo])
-def find(todo_filter: TodoFilter = Depends(), todo_repository: TodoRepository = Depends(create_todo_repository)):
-    with todo_repository as repo:
-        return repo.get(todo_filter)
