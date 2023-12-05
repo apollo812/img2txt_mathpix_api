df0f1c332c7ab093ad20a0ed5b53decbaf9f3970
OridosAI <146871237+oridosai@users.noreply.github.com>
2023-12-05 07:51:18 -0800

    Basic (#1)
    * feat: infra
    
    * refactor: infra
    
    * fix: infra docker file error
    
    * feat: mathpix api integration
    
    * refactor: use the latest version api of mathpix
    
    ---------
    
    Co-authored-by: apollo812 <goodluckbusiness0101@gmail.com>

---
+++ api/main.py
@@ -0,0 +1,52 @@
+import io
+import os
+import sys
+
+# Get the current script's directory
+current_script_directory = os.path.dirname(os.path.abspath(__file__))
+
+# Get the project root path
+project_root = os.path.abspath(os.path.join(current_script_directory, os.pardir))
+
+# Append the project root and current script directory to the system path
+sys.path.append(project_root)
+sys.path.append(current_script_directory)
+
+from fastapi import Depends, FastAPI, File, Response, UploadFile
+from starlette.responses import RedirectResponse
+from starlette.status import HTTP_201_CREATED
+
+from PIL import Image
+import scripts.mathpix as mathpix
+import json
+
+# Create a FastAPI application
+app = FastAPI(swagger_ui_parameters={"tryItOutEnabled": True})
+
+
+# Define a route to handle the root endpoint and redirect to the API documentation
+@app.get("/")
+async def root():
+    return RedirectResponse(app.docs_url)
+
+@app.post("/img2txt", status_code=HTTP_201_CREATED)
+async def i2t(file: UploadFile = File(...)):
+    contents = await file.read()
+
+    r = mathpix.latex({
+        'src': mathpix.image_content(contents), #mathpix.image_uri('./api/images/algebra.jpg'),
+        'ocr': ['math', 'text'],
+        'formats': ['text', 'latex_styled', 'asciimath', 'mathml', 'latex_simplified'],
+        'format_options': {
+            'text': {
+                'transforms': ['rm_spaces', 'rm_newlines'],
+                'math_delims': ['$', '$']
+            },
+            'latex_styled': {'transforms': ['rm_spaces']}
+        }
+    })
+
+    print(json.dumps(r, indent=4, sort_keys=True))
+    # assert(r['latex_simplified'] == '12 + 5 x - 8 = 12 x - 10')
+
+    return {"result": json.dumps(r, indent=4, sort_keys=True)}
---
+++ api/scripts/mathpix.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import os
+import base64
+import requests
+import json
+
+#
+# Common module for calling Mathpix OCR service from Python.
+#
+# N.B.: Set your credentials in environment variables APP_ID and APP_KEY,
+# either once via setenv or on the command line as in
+# APP_ID=my-id APP_KEY=my-key python3 simple.py 
+#
+
+env = os.environ
+app = env['MATHPIX_APP_ID']
+key = env['MATHPIX_APP_KEY']
+
+default_headers = {
+    'app_id': app,
+    'app_key': key,
+    'Content-type': 'application/json'
+}
+
+service = 'https://api.mathpix.com/v3/text'
+
+#
+# Return the base64 encoding of an image with the given filename.
+#
+def image_uri(filename):
+    image_data = open(filename, "rb").read()
+    return "data:image/jpg;base64," + base64.b64encode(image_data).decode()
+
+#
+# Return the base64 encoding of an image with the content.
+#
+def image_content(contents):
+    encoded_string = base64.b64encode(contents).decode()
+    return f"data:image/jpg;base64,{encoded_string}"
+
+#
+# Call the Mathpix service with the given arguments, headers, and timeout.
+#
+def latex(args, headers=default_headers, timeout=30):
+    r = requests.post(service,
+        data=json.dumps(args), headers=headers, timeout=timeout)
+    return json.loads(r.text)
