bebe54d808f88ac191024172558debd21f28c165
apollo812 <goodluckbusiness0101@gmail.com>
2023-12-05 00:34:08 -0500

    feat: mathpix api integration

--- api/main.py
+++ api/main.py
@@ -16,6 +16,10 @@ from fastapi import Depends, FastAPI, File, Response, UploadFile
 from starlette.responses import RedirectResponse
 from starlette.status import HTTP_201_CREATED
 
+from PIL import Image
+import scripts.mathpix as mathpix
+import json
+
 # Create a FastAPI application
 app = FastAPI(swagger_ui_parameters={"tryItOutEnabled": True})
 
@@ -24,3 +28,25 @@ app = FastAPI(swagger_ui_parameters={"tryItOutEnabled": True})
 @app.get("/")
 async def root():
     return RedirectResponse(app.docs_url)
+
+@app.post("/img2txt", status_code=HTTP_201_CREATED)
+async def i2t(file: UploadFile = File(...)):
+    contents = await file.read()
+
+    r = mathpix.latex({
+        'src': mathpix.image_content(contents), #mathpix.image_uri('./api/images/algebra.jpg'),
+        'ocr': ['math', 'text'],
+        'formats': ['text', 'latex_styled', 'asciimath', 'mathml', 'latex_simplified'],
+        'format_options': {
+            'text': {
+                'transforms': ['rm_spaces', 'rm_newlines'],
+                'math_delims': ['$', '$']
+            },
+            'latex_styled': {'transforms': ['rm_spaces']}
+        }
+    })
+
+    print(json.dumps(r, indent=4, sort_keys=True))
+    # assert(r['latex_simplified'] == '12 + 5 x - 8 = 12 x - 10')
+
+    return {"result": json.dumps(r, indent=4, sort_keys=True)}
